e52a7091efd1c7677466b35709eccea101e271e4
UsamaAbbasi12 <usamaabbasi199@gmail.com>
2025-08-20 17:48:42 +0500

    Database connection

--- src/main/java/com/ecommerce/platform/model/CategoryModal.java
+++ src/main/java/com/ecommerce/platform/model/CategoryModal.java
@@ -1,6 +1,14 @@
 package com.ecommerce.platform.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "categories")
 public class CategoryModal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String name;
 
